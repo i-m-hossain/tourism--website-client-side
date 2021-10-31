6ed9468aee08e6b27e039177cee775577b922701
mdimran1409036 <mdimran1409036@gmail.com>
2021-10-31 06:09:32 +0600

    conditional header elements are added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -11,21 +11,27 @@ const Header = () => {
                 <Navbar.Brand as={Link} to="/">Happy events</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link as={Link} to="/services/add">Add A New Service</Nav.Link>
-                        
-                        <NavDropdown title="Orders" id="collasible-nav-dropdown">
-                            <NavDropdown.Item  as={Link} to="/my-orders">My Orders</NavDropdown.Item>
-                            <NavDropdown.Item as={Link} to="/manage-orders">Manage All Orders</NavDropdown.Item>
-                        </NavDropdown>
-                        
-                    </Nav>
+                    {
+                        user.email && <Nav className="ms-auto">
+                            
+
+                            <NavDropdown title={user?.displayName} id="collasible-nav-dropdown">
+                                <NavDropdown.Item as={Link} to="/my-orders">My Orders</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/manage-orders">Manage All Orders</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/services/add">Add A New Service</NavDropdown.Item>
+                            </NavDropdown>
+
+                        </Nav>
+                    }
                     <Nav>
-                        <Nav.Link href="/login">Login</Nav.Link>
-                        <Nav.Link onClick={logout}>Logout</Nav.Link>
-                        <Nav.Link eventKey={2} href="#memes">
-                            {user?.displayName}
-                        </Nav.Link>
+                        {
+                            !user.email ?
+                                <Nav.Link href="/login">Login</Nav.Link> :
+                                <Nav.Link onClick={logout}>Logout</Nav.Link>
+
+                        }
+
+
                     </Nav>
                 </Navbar.Collapse>
             </Container>
