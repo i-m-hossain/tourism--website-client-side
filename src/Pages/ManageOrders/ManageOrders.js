471ac36266f82c659bd50216e43495eac56eb359
mdimran1409036 <mdimran1409036@gmail.com>
2021-10-31 09:17:28 +0600

    manage all orders added and delete acttion is enabled

--- src/Pages/ManageOrders/ManageOrders.js
+++ src/Pages/ManageOrders/ManageOrders.js
@@ -1,9 +1,53 @@
-import React from 'react';
 
+
+import axios from 'axios';
+import { Table, Button } from 'react-bootstrap';
+import useOrders from '../../hooks/useOrders';
 const ManageOrders = () => {
+    const [orders, setOrders] = useOrders()
+    const handleDeleteOrder = (id) => {
+        const confirm = window.confirm('Are you sure want to delete?');
+        if (confirm) {
+            return axios.delete(`http://localhost:5000/orders/${id}`)
+                .then(res => {
+                    if (res.data.deletedCount > 0) {
+                        const restOrders = orders.filter(order => order._id !== id)
+                        console.log(restOrders);
+                        console.log(id);
+                        setOrders(restOrders)
+                    }
+                })
+        }
+    }
+
     return (
-        <div>
-            this is manage order
+        <div className="m-5">
+           {
+                orders.length > 0 ? <Table striped bordered hover size="sm" >
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Image</th>
+                            <th> Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map((order, index) => <tr key={order._id}>
+                                <td>{index + 1}</td>
+                                <td>{order.service.title}</td>
+                                <td><img src={order.service.img} height="60px" width="60" alt="" /></td>
+                                <td><Button variant="danger" onClick={() => handleDeleteOrder(order._id)}>Delete</Button></td>
+                            </tr>)
+
+                        }
+
+                    </tbody>
+                </Table>
+                :
+                <h4>Orders not found</h4>
+           }
         </div>
     );
 };
---
+++ src/hooks/useOrders.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+const useOrders =() =>{
+    const [orders, setOrders] = useState([])
+    useEffect(() => {
+        axios.get(`http://localhost:5000/orders`)
+            .then(res => setOrders(res.data))
+    }, [])
+    return [orders, setOrders]
+}
+export default useOrders;
